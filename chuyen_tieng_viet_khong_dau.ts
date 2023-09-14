cfd69795252193d56891e2253a1428a7627e4a70
ThuHong <thuhong0501@gmail.com>
2023-09-14 18:08:23 +0700

    ba tap String

---
+++ chuyen_tieng_viet_khong_dau.ts
@@ -0,0 +1,15 @@
+
+const str = 'Lê Thị Thu Hồng';
+
+const newstr: string = str.replace('à', 'a').replace('á', 'a').replace('ạ', 'a').replace('ã', 'a').
+replace('ă', 'a').replace('ằ', 'a').replace('ắ', 'a').replace('ặ', 'a').replace('ẵ', 'a').
+replace('â', 'a').replace('ầ', 'a').replace('ấ', 'a').replace('ậ', 'a').replace('ẫ', 'a').
+replace('è', 'e').replace('é', 'e').replace('ẹ', 'e').replace('ẽ', 'e').
+replace('ê', 'e').replace('ề', 'e').replace('ế', 'e').replace('ệ', 'e').replace('ễ', 'e').
+replace('ì', 'i').replace('í', 'i').replace('ị', 'i').replace('ĩ', 'i').
+replace('ò', 'o').replace('ó', 'o').replace('ọ', 'o').replace('õ', 'o').
+replace('ô', 'o').replace('ồ', 'o').replace('ố', 'o').replace('ộ', 'o').replace('ỗ', 'o').
+replace('ơ', 'o').replace('ờ', 'o').replace('ớ', 'o').replace('ợ', 'o').replace('ỡ', 'o').
+replace('ù', 'u').replace('ú', 'u').replace('ụ', 'u').replace('ũ', 'u').
+replace('ư', 'u').replace('ừ', 'u').replace('ứ', 'u').replace('ự', 'u').replace('ữ', 'u');
+console.log(newstr);
